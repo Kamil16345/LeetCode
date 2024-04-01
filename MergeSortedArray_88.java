dac762ab72f07f0b70897a5931d56b5947c994ab
Kamil <KamilArkit@interia.pl>
2024-04-01 23:16:59 +0200

    merge-sorted-array(problem 88)

--- MergeSortedArray_88.java
+++ MergeSortedArray_88.java
@@ -1,13 +1,23 @@
-class Solution {
-public void merge(int[] nums1, int m, int[] nums2, int n) {
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class MergeSortedArray_88 {
+    public static void main(String[] args) {
+        int[] nums1 = new int[]{1,2,3,0,0,0};
+        int m = 3;
+        int[] nums2 = new int[]{2,5,6};
+        int n = 3;
+        merge(nums1, m, nums2, n);
+    }
+    public static void merge(int[] nums1, int m, int[] nums2, int n) {
         ArrayList<Integer> mergedList = new ArrayList<>();
         int[] removedZeroes1 = removeTrailingZeroes(nums1);
         int[] removedZeroes2 = removeTrailingZeroes(nums2);
         for(int element : removedZeroes1){
-                mergedList.add(element);
+            mergedList.add(element);
         }
         for(int element : removedZeroes2){
-                mergedList.add(element);
+            mergedList.add(element);
         }
         Collections.sort(mergedList);
         for (int i = 0; i < mergedList.size(); i++) {
@@ -15,7 +25,7 @@ public void merge(int[] nums1, int m, int[] nums2, int n) {
         }
     }
 
-    public int[] removeTrailingZeroes(int[] numbers) {
+    public static int[] removeTrailingZeroes(int[] numbers) {
         int lastNonZeroIndex = numbers.length;
         for (int i = numbers.length-1; i>=0; i--) {
             if(numbers[i] != 0){
