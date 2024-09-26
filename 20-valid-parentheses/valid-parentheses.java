3633bc2040d5a2407e008c68ef1adcb7d45858cb
Kamil Arkit <75177619+Kamil16345@users.noreply.github.com>
2024-09-26 23:33:16 +0200

    Time: 3 ms (12.12%) | Memory: 41.9 MB (11.87%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,25 +1,26 @@
 class Solution {
     public boolean isValid(String s) {
         Stack<Character> chars = new Stack<>();
+        if (s.charAt(0) == '}' || s.charAt(0) == ')' || s.charAt(0) == ']') {
+            return false;
+        }
         for (Character ch : s.toCharArray()) {
             if (ch.equals('{') || ch.equals('(') || ch.equals('[')) {
                 chars.push(ch);
-            } else if (ch.equals('}') || ch.equals(')') || ch.equals(']')) {
-                if(chars.size()>0){
-                    if (chars.peek().equals('{') && ch.equals('}')) {
-                        chars.pop();
-                    } else if (chars.peek().equals('(') && ch.equals(')')) {
-                        chars.pop();
-                    } else if (chars.peek().equals('[') && ch.equals(']')) {
-                        chars.pop();
-                    }else{
-                        return false;
-                    }
-                }else{
+            } else if ((ch.equals('}') || ch.equals(')') || ch.equals(']')) && chars.size() > 0) {
+                if (chars.peek().equals('{') && ch.equals('}')) {
+                    chars.pop();
+                } else if (chars.peek().equals('(') && ch.equals(')')) {
+                    chars.pop();
+                } else if (chars.peek().equals('[') && ch.equals(']')) {
+                    chars.pop();
+                } else {
                     return false;
                 }
+            } else {
+                return false;
             }
         }
-        return chars.size()==0;
+        return chars.size() == 0;
     }
 }
