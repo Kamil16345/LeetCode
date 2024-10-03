53ce9b4ea9d96a7647b84b26d3d6cfc198cf28de
Kamil Arkit <75177619+Kamil16345@users.noreply.github.com>
2024-10-03 21:25:34 +0200

    Time: 14 ms (32.13%) | Memory: 45.1 MB (42.52%) - LeetSync

--- 383-ransom-note/ransom-note.java
+++ 383-ransom-note/ransom-note.java
@@ -1,29 +1,15 @@
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
-        HashMap<Character, Integer> magazineMap = new HashMap<>();
-        for(int i=0; i<magazine.length();i++){
-            if(magazineMap.containsKey(magazine.charAt(i))){
-                int val = magazineMap.get(magazine.charAt(i));
-                val++;
-                magazineMap.put(magazine.charAt(i),val);
-            }else{
-                magazineMap.put(magazine.charAt(i),1);
-            }
+        HashMap<Character,Integer>magazineMap = new HashMap<>();
+        for(Character ch : magazine.toCharArray()){
+            magazineMap.put(ch, magazineMap.getOrDefault(ch, 0)+1);
         }
-        for(int i=0; i<ransomNote.length();i++){
-            boolean containsKey = magazineMap.containsKey(ransomNote.charAt(i));
-            Integer integer = magazineMap.get(ransomNote.charAt(i));
-            if(magazineMap.containsKey(ransomNote.charAt(i)) && magazineMap.get(ransomNote.charAt(i))>0){
-                int val = magazineMap.get(ransomNote.charAt(i));
-                val--;
-                magazineMap.put(ransomNote.charAt(i),val);
-                if(i==ransomNote.length()-1){
-                    return true;
-                }
-            }else{
+        for(Character ch : ransomNote.toCharArray()){
+            if(!magazineMap.containsKey(ch)|| magazineMap.get(ch)<=0){
                 return false;
             }
+            magazineMap.put(ch, magazineMap.get(ch)-1);
         }
-        return false;
+        return true;
     }
 }
