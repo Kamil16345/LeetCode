0e0f27f8d9ef165f87d31ec16c0b0696e95800c0
Kamil Arkit <75177619+Kamil16345@users.noreply.github.com>
2024-10-27 12:08:24 +0100

    Time: 6 ms (33.46%) | Memory: 43.3 MB (98.71%) - LeetSync

--- 9-palindrome-number/palindrome-number.java
+++ 9-palindrome-number/palindrome-number.java
@@ -1,5 +1,8 @@
 class Solution {
     public boolean isPalindrome(int x) {
+        if(x<0){
+            return false;
+        }
         String stringX= String.valueOf(x);
         int start = 0;
         int end = stringX.length()-1;
